d0729be504fe737a3ac512b45152e75a1efecbc6
Keagan <keaganbotha11@gmail.com>
2025-08-04 17:02:19 +0200

    first commit

---
+++ profile-card/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ProfileCard from './ProfileCard'
+
+export default function App() {
+
+  return(
+    <div style={{
+      display: 'flex', flexDirection: 'column', alignItems: 'center',gap: '20px',paddingTop: '30px'}}>
+      <ProfileCard
+        image="https://avatar.iran.liara.run/public/48"
+        name="Keagan Botha"
+        title="Web developer"
+        bio="A passionate web developer who loves developing websites."/>
+      <ProfileCard
+        image="https://avatar.iran.liara.run/public/44"
+        name="Aaron Botha"
+        title="UI/UX Designer"
+        bio="keaganbotha11@gmail.com."/>
+        
+    </div>
+   
+  );
+
+
+}
---
+++ profile-card/src/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import './ProfileCard.css';
+
+export default function ProfileCard(props){
+    return(
+        <div className="profile-card">
+            <img src={props.image} alt="Profile" className="profile-image" />
+            <h2>{props.name}</h2>
+            <h4>{props.title}</h4>
+            <p>{props.bio}</p>
+        </div>
+    );
+
+}
